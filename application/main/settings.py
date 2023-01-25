9f17b309ba0006729bafdf5ca669dfd630355313
derfacn <nikitalupalo145@gmail.com>
2023-01-25 21:55:13 +0100

    settings fix

--- application/main/settings.py
+++ application/main/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-9k&s*&uzg#4+rnudfh+!b8u5tki7y_26-954_s^@g8)6=utgzm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "derfacn.pythonanywhere.com"]
 
 
 # Application definition
