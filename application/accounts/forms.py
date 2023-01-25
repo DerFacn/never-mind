231a3c8fc0bb9cb5737a9a312d64b6fc9084c184
derfacn <nikitalupalo145@gmail.com>
2023-01-25 21:54:18 +0100

    validator fix 0.2

--- application/accounts/forms.py
+++ application/accounts/forms.py
@@ -1,14 +1,13 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .validators import username_validator, email_validator
-from django.core.validators import EmailValidator
+from .validators import username_validator, email_validator, letter_validator
 
 class SignUpForm(UserCreationForm):
-    username = forms.CharField(label="Логін", max_length=30, required=True,validators=[username_validator,], error_messages={'required': "Це поле обов'язкове!"})
-    first_name = forms.CharField(label="Ім'я", max_length=30, required=True, error_messages={'required': "Це поле обов'язкове!"})
-    last_name = forms.CharField(label="Прізвище", max_length=30, required=True, error_messages={'required': "Це поле обов'язкове!"})
-    email = forms.EmailField(label="Електронна адреса", max_length=320,required=True,validators=[email_validator,], error_messages={'required': "Це поле обов'язкове!"})
+    username = forms.CharField(label="Username", max_length=30, required=True,validators=[username_validator,], error_messages={'required': "Це поле обов'язкове!"})
+    first_name = forms.CharField(label="First Name", max_length=30, required=True,validators=[letter_validator,], error_messages={'required': "Це поле обов'язкове!"})
+    last_name = forms.CharField(label="Last Name", max_length=30, required=True,validators=[letter_validator,], error_messages={'required': "Це поле обов'язкове!"})
+    email = forms.EmailField(label="Email", max_length=320,required=True,validators=[email_validator,], error_messages={'required': "Це поле обов'язкове!"})
 
     class Meta:
         model = User
--- application/accounts/validators.py
+++ application/accounts/validators.py
@@ -2,15 +2,22 @@ from django.core.exceptions import ValidationError
 import re
 
 def username_validator(username):
-    pat = re.compile(r"[A-Za-z0-9]+")
+    pat = re.compile(r"[A-Za-z0-9_.]+")
     if re.fullmatch(pat, username):
         return username
     else:
-        raise ValidationError("Invalid username!")
+        raise ValidationError("Invalid username!(english only _ and .")
 
 def email_validator(email):
     pat = re.compile(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
     if re.fullmatch(pat, email):
         return email
     else:
-        raise ValidationError("Invalid email!")
+        raise ValidationError("Invalid email!(only _ . - + and english letters")
+
+def letter_validator(value):
+    pat = re.compile(r"[a-zA-Z]")
+    if re.fullmatch(pat, value):
+        return value
+    else:
+        raise ValidationError("Invalid value!(only english letters)")
